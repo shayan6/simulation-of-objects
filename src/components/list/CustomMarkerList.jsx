8e7ecedae2eae9d919732fc9cefc9d823f366e50
Shayan Shaikh <shayanshaikh996@gmail.com>
2023-12-14 21:18:01 +0200

    Added fancy card

--- src/components/list/CustomMarkerList.jsx
+++ src/components/list/CustomMarkerList.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { List } from "antd";
+import { List, Avatar, Space } from "antd";
 
 const MarkerList = ({ markers }) => (
   <List
     dataSource={markers}
     renderItem={(item, index) => (
       <List.Item>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div
-            style={{ marginRight: 8 }}
-            dangerouslySetInnerHTML={{ __html: item.icon }}
-          />
-          <span style={{ marginRight: 8 }}>Speed: {item.speed}</span>
-        </div>
+        <Space>
+          <Avatar
+            size={40}
+            shape="square"
+            style={{ border: `1px solid ${item.color}`, backgroundColor: '#dddd' }}
+          >
+            <span dangerouslySetInnerHTML={{ __html: item.icon }} />
+          </Avatar>
+          <div>
+            <p style={{ margin: 0, fontSize: 16, fontWeight: "bold" }}>
+              Marker {index + 1}
+            </p>
+            <p style={{ margin: 0 }}>Speed: {item.speed}</p>
+          </div>
+        </Space>
       </List.Item>
     )}
   />
