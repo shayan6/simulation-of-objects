02ea17ad89fddab274f2dc0e1d3a9b84f12509a3
Shayan Shaikh <shayanshaikh996@gmail.com>
2023-12-21 14:34:45 +0200

    Remove depreciated tabpane

--- src/components/controls/MapControls.jsx
+++ src/components/controls/MapControls.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import MarkerList from "../list/CustomMarkerList";
 import { Tabs } from "antd";
-import TabPane from "antd/es/tabs/TabPane";
 import MapFilters from "../filters/MapFilters";
 import { useDispatch, useSelector } from "react-redux";
 import { addMarker } from "../../actions/markersSlice"; 
 
 const MapControls = () => {
+
   const dispatch = useDispatch();
   const markers = useSelector((state) => state.markers.list);
   const setMarkers = (newMarkers) => {
@@ -14,16 +14,22 @@ const MapControls = () => {
     dispatch(addMarker(newMarkers));
   };
 
+  const items = [
+    {
+      key: '1',
+      label: 'Custom marker list',
+      children: <MarkerList markers={markers} />,
+    },
+    {
+      key: '2',
+      label: 'Add new marker',
+      children: <MapFilters markers={markers} setMarkers={setMarkers} />,
+    },
+  ];
+  
   return (
     <div className="controls-btn">
-      <Tabs defaultActiveKey="1">
-        <TabPane tab="Add new marker" key="1">
-          <MapFilters markers={markers} setMarkers={setMarkers} />
-        </TabPane>
-        <TabPane tab="Custom marker list" key="2">
-          <MarkerList markers={markers} />
-        </TabPane>
-      </Tabs>
+      <Tabs defaultActiveKey="1" items={items} />;
     </div>
   );
 };
